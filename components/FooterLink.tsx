8078df4eb9a12eee16922c53af98c0c24443f6c3
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-08 13:00:27 +0300

    add: footer link

---
+++ components/FooterLink.tsx
@@ -0,0 +1,37 @@
+import { Button, HStack, Link } from "@chakra-ui/react"
+import type { FC } from 'react'
+
+type IProps = {
+    postLink:string;
+    postLinkTitle:string
+}
+
+const FooterLink:FC<IProps> = ({ postLink, postLinkTitle }) => {
+    return (
+        <HStack
+            marginTop='5%'
+        >
+            <Link 
+                href={postLink}
+                _hover={{
+                    textDecoration:'none'
+                }}
+            >
+                <Button 
+                    color='whatsapp.400' 
+                    variant='outline'
+                    borderColor='whatsapp.400'
+                    _hover={{
+                        backgroundColor: 'whatsapp.400',
+                        color: 'white'
+                    }}
+                
+                >
+                    {postLinkTitle}
+                </Button>
+            </Link>
+        </HStack>
+    )
+}
+
+export default FooterLink
