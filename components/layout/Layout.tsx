ac58fe58a6520b645c6e4d8c983e09c72f7238f4
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-21 17:51:53 +0300

    update layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -13,13 +13,12 @@ const Layout:FC<IProps> = ({children}) => {
             <title>Mert Sabinov</title>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta name='description' content='portfolio' />
+                <meta name='description' content='I am a Software Developer passionate about developing ideas in open source communities. Currently learning React, Next.js, TypeScript, Go, React Native, graphql, prisma, MongoDB and running open source projects. I want to work with technologies like React, Next.js, TypeScript, Go, React Native, graphql, prisma, MongoDB As a developer, most of my time is spent creating projects and learning new things every day.' />
                 <meta name='keywords' content='mert, mertsabinov, sabinov, mert sabinov' />
                 <meta name="author" content="Mert Sabinov" />
             </Head>
             <Box
-                display='flex'
-                flexDirection='row'
+                bgColor='#2D2D2D'
             >
                 {children}
             </Box>
