bd79b050c569dd1af7ffde3db14d16a0743429f1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-22 11:33:22 +0300

    delete: navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Box, ChakraProvider, HStack } from '@chakra-ui/react'
-import Navbar from '../components/Navbar'
 import Layout from '../components/layout/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
         <Layout>
-          <Navbar />
           <Component {...pageProps} />
         </Layout>
    </ChakraProvider>
