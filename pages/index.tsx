2dc1d90146333a5ce997374fc0a8726b13d117cc
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-22 11:34:49 +0300

    update: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,18 @@
-import {
-  useMediaQuery,
-  VStack,
-  Text,
-  Tag,
-  HStack,
-  Link,
-  Box
-} from '@chakra-ui/react'
-import PageLayout from '../components/layout/PageLeyout'
+import AboutSide from "../components/LandingPage/AboutSide"
+import HeaderSide from "../components/LandingPage/HeaderSide"
+import PortfolioSide from "../components/LandingPage/PortfolioSide"
+import PageLayout from "../components/layout/PageLeyout"
 
-const Home = () => {
-  const [isMobile] = useMediaQuery('(min-width: 768px)')
-  return (
-    <PageLayout pageTitle='Mert Sabinov'>
-      <VStack
-        color='white'
-      >
-        <Text
-          fontSize='xx-large'
+const index = () => {
+    return (
+        <PageLayout
+            pageTitle="Mert Sabinov"
         >
-          Mert Sabinov
-        </Text>
-        <Text
-          fontSize='large'
-          textAlign='justify'
-        >
-          I am a Software Developer passionate about developing ideas in open source communities. 
-          Currently learning <Tag>React, Next.js, TypeScript, Go, React Native, graphql, prisma, MongoDB</Tag> and running open source projects. 
-          I want to work with technologies like React, Next.js, TypeScript, Go, React Native, graphql, prisma, MongoDB As a developer, 
-          most of my time is spent creating projects and learning new things every day.
-        </Text>
-      </VStack>
-      <Box
-        display='flex'
-        flexDirection={isMobile ? "row" : "column"}
-        marginTop='5%'
-        color='whatsapp.400'
-        justifyContent='space-evenly'
-      >
-        <Link href='https://github.com/mertsabinov'>
-          <Text>
-            GitHub
-          </Text>
-        </Link>
-        <Link href='https://www.linkedin.com/in/mertsabinov/'>
-          <Text>
-            Linkedin
-          </Text>
-        </Link>
-        <Link href='https://play.google.com/store/apps/dev?id=4780021152747475031&gl'>
-          <Text>
-            Google Play
-          </Text>
-        </Link>
-      <Link href='https://twitter.com/mertsabinov'>
-          <Text>
-            Twitter 
-          </Text>
-      </Link>
-      <Link href='https://medium.com/@mertsabinov'>
-          <Text>
-            Medium 
-          </Text>
-      </Link>
-      <Link href='https://www.reddit.com/user/mertsabinov'>
-          <Text>
-            Reddit
-          </Text>
-      </Link>  
-      </Box>
-    </PageLayout>
-  )
+            <HeaderSide />
+            <AboutSide />
+            <PortfolioSide/>
+        </PageLayout>
+    )
 }
 
-export default Home
+export default index
