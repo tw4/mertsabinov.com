17cae70ab34802a43f990ee7eef64b461cbc2b38
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-01-20 13:16:53 +0300

    update: about me side

--- components/LandingPage/AboutSide.tsx
+++ components/LandingPage/AboutSide.tsx
@@ -1,46 +1,54 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, List, ListItem } from "@chakra-ui/react";
 
 const AboutSide = () => {
   return (
-    <Box color="white" fontStyle="italic" textAlign="justify">
-      <Text fontSize="5xl" fontWeight="bold" fontStyle="normal">
-        About me
+    <Box fontSize="x-large" textColor="white">
+      <Heading as="h1">About me</Heading>
+      <Text>
+        I am a highly skilled and experienced software developer with a focus on
+        creating performant and user-friendly web applications. I have extensive
+        experience with technologies such as React, Next.js, TypeScript. I am
+        always eager to learn new technologies and programming languages,
+        currently my interest are GoLang and TypeScript.
       </Text>
-      <Text fontSize="2xl" marginTop="2.5">
-        I am a front-end developer with a passion for solving product problems
-        with technical solutions. I am proficient in technologies such as React,
-        Next.js, TypeScript, HTML, and CSS and have experience in developing web
-        applications.
-      </Text>
-      <Text fontSize="4xl" marginTop="2.5">
-        My Projects
-      </Text>
-      <Text fontSize="3xl" fontWeight="bold" marginTop="2.5">
-        Profilecard.co
-      </Text>
-      <Text fontSize="2xl">
-        A web application that allows users to create their own personal profile
-        pages and share links such as social media accounts. Developed using
-        React, Firebase, TypeScript and stitches for UI library. Improved
-        front-end development skills and developed a reusable UI library.
-      </Text>
-      <Text fontSize="3xl" fontWeight="bold" marginTop="2.5">
-        Workflow Premium
-      </Text>
-      <Text fontSize="2xl">
-        A mobile app that allows users to track and save their daily and monthly
-        earnings. Developed using React Native and local storage for user data
-        management. Gained experience in mobile development and improved
-        understanding of data management and storage.
-      </Text>
-      <Text fontSize="3xl" marginTop="2.5">
-        Contact Me
-      </Text>
-      <Text fontSize="2xl">
-        I am a team player who never gives up on learning and loves to share my
-        knowledge around. If you are looking for a motivated and skilled
-        front-end developer to join your team, feel free to contact me.
-      </Text>
+      <Heading as="h4" marginTop="2.5%">
+        My Professional Experience
+      </Heading>
+      <List styleType="disc">
+        <ListItem>
+          Developing and maintaining high-performing and user-friendly web
+          applications using technologies such as React, Next.js, and
+          TypeScript.
+        </ListItem>
+        <ListItem>
+          Implementing modern web development best practices and following
+          industry standards to deliver high-quality, production-ready code.
+        </ListItem>
+        <ListItem>
+          Collaborating with cross-functional teams to deliver projects on time
+          and within budget.
+        </ListItem>
+        <ListItem>
+          Continuously learning and staying up-to-date with the latest
+          technologies and programming languages, with a current focus on GoLang
+          and TypeScript.
+        </ListItem>
+        <ListItem>
+          Strong educational background in Computer Science and relevant
+          coursework and projects.
+        </ListItem>
+        <ListItem>
+          Strong problem-solving skills, attention to detail, and ability to
+          work well under pressure.
+        </ListItem>
+        <ListItem>
+          Excellent communication and team collaboration skills.
+        </ListItem>
+        <ListItem>
+          Proven ability to consistently deliver high-quality products and
+          contribute to the success of the organization.
+        </ListItem>
+      </List>
     </Box>
   );
 };
