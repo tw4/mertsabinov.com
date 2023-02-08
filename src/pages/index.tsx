783d7adc59a889feb048dd2437229a0219b9ee77
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-09 00:40:30 +0300

    fix: responsive card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
             Hello 👋 I`m a Software Developer Mert Sabinov
           </h1>
         </div>
-        <h2 className={styles.h2}>Projects I'm currently working on</h2>
+        <h2 className={styles.h2}>Projects I`m currently working on</h2>
         <CardGroup />
         <h2 className={styles.h2}>Open source projects of my choice</h2>
         <RepositoryCardGroup />
