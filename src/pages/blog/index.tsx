4deb373777a93ee3b09c23c940291204acc68788
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-09 15:26:35 +0300

    add: route blog

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import BlogList from "@/components/blogList/BlogList";
+import Layout from "@/components/layout/Layout";
+
+const Blog = () => {
+  return (
+    <Layout>
+      <BlogList />
+    </Layout>
+  );
+};
+
+export default Blog;
