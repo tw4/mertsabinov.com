05defc8033504e76da6108d02f0435b2d5587431
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-13 18:24:56 +0300

    update: config path

--- src/components/blogList/BlogList.tsx
+++ src/components/blogList/BlogList.tsx
@@ -1,4 +1,4 @@
-import { API } from "@/config";
+import { API } from "../../../config";
 import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import styles from "./BlogList.module.css";
