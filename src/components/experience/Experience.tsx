0aaf1fbacfd42feb74a368feb3c1ac7f34af20ac
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-14 17:44:22 +0300

    add: markdown support

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,39 +1,34 @@
 import styles from "./Experience.module.css";
-import {useEffect, useState} from "react";
-import {getExperience} from "@/api";
+import { useEffect, useState } from "react";
+import { getExperience } from "@/api";
+import ReactMarkdown from "react-markdown";
 
 const Experience = () => {
-  const [experience, setExperience] = useState<any[]>([])
+  const [experience, setExperience] = useState<any[]>([]);
 
   useEffect(() => {
-    getExperience().then(items => {
-      setExperience(items)
-    })
-  },[])
+    getExperience().then((items) => {
+      setExperience(items);
+    });
+  }, []);
   return (
     <div className={styles.container}>
       <h2>EXPERIENCE</h2>
       <div>
-        {
-          experience.map((item) => {
-            return (
-                <div key={item} className={styles.timeLineContainer}>
-                  <div className={styles.timeLinePoint}></div>
-                  <h3>
-                    {item.fields.title}
-                  </h3>
-                  <ul>
-                    <li>
-                      {item.fields.description}
-                    </li>
-                    <li>
-                      <a href={`${item.fields.projectUrl}`}>Link</a>
-                    </li>
-                  </ul>
-                </div>
-            )
-          })
-        }
+        {experience.map((item) => {
+          return (
+            <div key={item} className={styles.timeLineContainer}>
+              <div className={styles.timeLinePoint}></div>
+              <h3>{item.fields.title}</h3>
+              <ul>
+                <ReactMarkdown>{item.fields.description}</ReactMarkdown>
+                <li>
+                  <a href={`${item.fields.projectUrl}`}>Link</a>
+                </li>
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
