b6e1232887e85301db35b997a7c1beb4a9ad2404
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-07-14 08:47:47 +0300

    add: contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+const Contat = () => {
+  return (
+    <div>
+      <iframe
+        className="w-full h-screen"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfC4uAeFoCIzYfIQLzk6zIsdyXoAsUODTUEp4qt3ered9jDhg/viewform?embedded=true"></iframe>
+    </div>
+  );
+};
+
+export default Contat;
