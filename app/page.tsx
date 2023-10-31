b69cab4a3c1433a855c30e7361cda96955244854
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-10-31 12:28:36 +0300

    add links

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,43 @@
-import SocialMediaItem from '@/components/SocialMediaItem';
-import { RiTwitterXFill, RiGithubFill } from 'react-icons/ri';
+import SocialMediaItem from "@/components/SocialMediaItem";
+import {
+  RiTwitterXFill,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+} from "react-icons/ri";
 
 const home = () => {
   return (
     <main>
-      <div className='flex flex-row justify-center items-center w-[100vw] h-[100vh] bg-gradient-to-r from-violet-600 to-indigo-600 '>
-        <div className='text-center'>
+      <div className="flex flex-row justify-center items-center w-[100vw] h-[100vh] bg-gradient-to-r from-violet-600 to-indigo-600 ">
+        <div className="text-center">
           <a
-            className='text-white hover:cursor-pointer hover:underline'
-            href='https://github.com/buzzspire'
-          >
-            <h1 className='text-3xl'>Mert Sabinov Stuff</h1>
+            className="text-white hover:cursor-pointer hover:underline"
+            href="https://github.com/buzzspire">
+            <h1 className="text-3xl">Mert Sabinov Stuff</h1>
           </a>
-          <p className='text-white'>
-            Creator of the BuzzSpire Ecosystem. Tech Entrepreneur. Full Stack
-            Developer
+          <p className="text-white">
+            Creator of the{" "}
+            <a href="https://www.buzzspire.net/" className="hover:underline">
+              @BuzzSpire
+            </a>{" "}
+            Ecosystem. Tech Entrepreneur. Full Stack Developer
           </p>
         </div>
-        <div className='absolute h-20 w-full flex flex-row items-end justify-center space-x-3 bottom-10 z-10'>
+        <div className="absolute h-20 w-full flex flex-row items-end justify-center space-x-3 bottom-10 z-10">
           <SocialMediaItem
-            icon={<RiTwitterXFill className='text-white' />}
-            link='https://x.com/mertsabinov'
+            icon={<RiTwitterXFill className="text-white" />}
+            link="https://x.com/mertsabinov"
+            title="X.com"
           />
           <SocialMediaItem
-            icon={<RiGithubFill className='text-white' />}
-            link='https://github.com/mertsabinov'
+            icon={<RiGithubFill className="text-white" />}
+            link="https://github.com/mertsabinov"
+            title="GitHub"
+          />
+          <SocialMediaItem
+            icon={<RiLinkedinBoxFill className="text-white" />}
+            link="https://linkedin.com/in/mertsabinov"
+            title="LinkedIn"
           />
         </div>
       </div>
--- components/SocialMediaItem.tsx
+++ components/SocialMediaItem.tsx
@@ -3,13 +3,15 @@ import { FC, ReactNode } from "react";
 interface SocialMediaItemProps {
   icon: ReactNode;
   link: string;
+  title: string;
 }
 
-const SocialMediaItem: FC<SocialMediaItemProps> = ({ icon, link }) => {
+const SocialMediaItem: FC<SocialMediaItemProps> = ({ icon, link, title }) => {
   return (
-    <div className="flex flex-row space-x-3">
-      <a className="text-xl" href={link}>
+    <div className="flex flex-row space-x-3 text-white hover:underline">
+      <a className="flex flex-row space-x-2 items-center text-lg" href={link}>
         {icon}
+        <p>{title}</p>
       </a>
     </div>
   );
