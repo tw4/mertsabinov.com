016513c262189d6e5fda59410d983a543c5d171a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2024-01-26 23:20:49 +0300

    add: only the last 5 posts are visible

--- app/components/Articles.tsx
+++ app/components/Articles.tsx
@@ -44,27 +44,30 @@ export const Articles: FC<ArticlesProps> = ({ lang = 'en-US' }) => {
       <div>
         <h1 className="text-3xl mb-5">latest posts</h1>
         <ul className="space-y-3 mb-10">
-          {items.map((item, index) => (
-            <div
-              className="flex flex-row justify-between text-end hover:cursor-pointer hover:underline hover:decoration-textHighlight"
-              key={index}
-            >
-              <p>
-                {new Date(item.pubDate).toLocaleDateString(lang, {
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                })}
-              </p>
-              <a
-                className="text-textHighlight"
-                href={item.link}
-                target={'_blank'}
-              >
-                <h3>{item.title}</h3>
-              </a>
-            </div>
-          ))}
+          {items.map(
+            (item, index) =>
+              index <= 5 && (
+                <div
+                  className="flex flex-row justify-between text-end hover:cursor-pointer hover:underline hover:decoration-textHighlight"
+                  key={index}
+                >
+                  <p>
+                    {new Date(item.pubDate).toLocaleDateString(lang, {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </p>
+                  <a
+                    className="text-textHighlight"
+                    href={item.link}
+                    target={'_blank'}
+                  >
+                    <h3>{item.title}</h3>
+                  </a>
+                </div>
+              )
+          )}
         </ul>
         <a
           className="text-textHighlight hover:underline hover:decoration-textHighlight"
