85da111591e86d1e4b2bf0158e687a197888e03e
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2024-01-27 12:04:22 +0300

    add: go to base path

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
-  const router = useRouter();
   useEffect(() => {
-    router.push('/tr');
+    window.location.href = '/tr';
   }, []);
   return <div></div>;
 }
